5983f28589db0dda24f5fbaf552e2895eb334730
Wolvrin12 <anouarchlih31@gmail.com>
2023-07-27 19:03:48 +0100

    setting firebase

---
+++ build/precache-manifest.ba9b2fa834a752e209268f8e4938f961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c98215ae5051c3fd431a2cb5b33dc33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e0754f03411d5d77cec",
+    "url": "/static/css/2.15284052.chunk.css"
+  },
+  {
+    "revision": "0c6cb9ec4cdf02826da6",
+    "url": "/static/css/main.875e6e08.chunk.css"
+  },
+  {
+    "revision": "3e0754f03411d5d77cec",
+    "url": "/static/js/2.aef8d4d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aef8d4d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6cb9ec4cdf02826da6",
+    "url": "/static/js/main.e9729750.chunk.js"
+  },
+  {
+    "revision": "46d6e46eb169154effff",
+    "url": "/static/js/runtime-main.ae5104b5.js"
+  }
+]);
